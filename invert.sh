38bd5425e29b7fc5ac2e39c4e963e372d169c72b
steventheworker <steventheworker@gmail.com>
2023-11-21 17:31:05 -0800

    fix inverted files having >>> size (dpi)

--- invert.sh
+++ invert.sh
@@ -1,9 +1,7 @@
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
    echo "No argument supplied"
    exit 1
 fi
-arg1=$1
 
 # Input PDF file
 input_pdf="$1"
@@ -17,12 +15,26 @@ temp_dir="temp_images"
 # Create a temporary directory if it doesn't exist
 mkdir -p "$temp_dir"
 
+
+# Calculate DPI based on output dimensions and desired quality
+dpis=$(convert -ping "$input_pdf" -format "%x\n" info:)
+dpi=`echo "${dpis}" | head -1` # first line only
+
+# standard sheet of paper
+# output_width=8.5
+# output_height=11
+
+# desired_width_pixels=$(echo "$output_width * $dpi / 1" | bc)
+# desired_height_pixels=$(echo "$output_height * $dpi / 1" | bc)
+
 # Convert each page of the PDF to an image and invert it
-convert -density 300 -quality 100 "$input_pdf" "$temp_dir/page_%04d.jpg"
+# convert -density "$dpi" -quality 100 "$input_pdf" -resize "${desired_width_pixels}x${desired_height_pixels}" "$temp_dir/page_%04d.jpg"
+convert -density "$dpi" -quality 100 "$input_pdf" "$temp_dir/page_%04d.jpg"
 mogrify -negate "$temp_dir"/*.jpg
 
 # Convert the inverted images back to a PDF
 convert "$temp_dir"/*.jpg "$output_pdf"
+
 
 # Clean up temporary files
 rm -r "$temp_dir"
